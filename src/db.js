666ef580136ed1eebd14e97a12f2885d24f42dd6
omeshF <omeshf@gmail.com>
2026-04-02 17:14:58 +0100

    update

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 // 🔴 VULN A02: Passwords stored in plaintext
 export const users = [
-  { id:1, username:"admin",   password:"adminpass",   email:"admin@corp.com",  role:"admin", salary:95000, ssn:"123-45-6789" },
-  { id:2, username:"alice",   password:"alice123",    email:"alice@corp.com",  role:"staff", salary:52000, ssn:"987-65-4321" },
-  { id:3, username:"bob",     password:"bobspassword",email:"bob@corp.com",    role:"staff", salary:48000, ssn:"456-78-9012" },
+  { id:1, username:"admin",   password:"password123",   email:"admin@corp.com",  role:"admin", salary:95000, ssn:"123-45-6789" },
+  { id:2, username:"alice",   password:"letmein",    email:"alice@corp.com",  role:"staff", salary:52000, ssn:"987-65-4321" },
+  { id:3, username:"bob",     password:"bob123",email:"bob@corp.com",    role:"staff", salary:48000, ssn:"456-78-9012" },
   { id:4, username:"charlie", password:"charlie99",   email:"charlie@corp.com",role:"dba",   salary:67000, ssn:"321-54-9876" },
 ];
 
